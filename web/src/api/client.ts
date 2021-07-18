df86862dfec8f9a9e5c73674bb2de8931c921d4e
Mark Skelton <mdskelton99@gmail.com>
2021-07-17 21:52:49 -0500

    Don't use fully qualified API URL

--- web/src/api/client.ts
+++ web/src/api/client.ts
@@ -2,5 +2,5 @@ import { ApolloClient, InMemoryCache } from "@apollo/client"
 
 export const client = new ApolloClient({
   cache: new InMemoryCache().restore(window.__APOLLO_STATE__),
-  uri: "http://localhost:4000/graphql",
+  uri: "/graphql",
 })
